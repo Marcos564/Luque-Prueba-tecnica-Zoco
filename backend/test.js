8ff7febe96f1cb0b52b890eec2f3d44574c57895
Marcos <marcos56482@gmail.com>
2026-05-08 11:05:25 -0300

    fix frontend structure

--- backend/test.js
+++
@@ -1,8 +0,0 @@
-import { getBarsInTucuman } from "./src/api/NO-VAgooglePlaces.js";
-
-const run = async () => {
-  const data = await getBarsInTucuman();
-  console.log(data.slice(0, 3));
-};
-
-run();
